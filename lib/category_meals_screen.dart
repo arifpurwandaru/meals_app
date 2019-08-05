78cd840b436ad96a018951ceb6bd298530f946cf
Arif Nazar Purwandaru <arifnazar.purwandaru@mitrais.com>
2019-08-05 14:50:01 +0700

    162. Adding a Meal Model And Data

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    final routeArgs = ModalRoute.of(context).settings.arguments as Map<String,String>;
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryId = routeArgs['id'];
     final categoryTitle = routeArgs['title'];
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text('The Recipes for the category!'),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {},
       ),
     );
   }
